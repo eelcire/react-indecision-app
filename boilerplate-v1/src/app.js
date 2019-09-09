dc02376ba07ed779d95f0d1a2d337207cecabc7b
eelcire <eelcire.eelcire@hotmail.com>
2019-09-09 10:38:54 -0500

    fourth commit

---
+++ boilerplate-v1/src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.scss';
+import 'normalize.css';
+
+ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
