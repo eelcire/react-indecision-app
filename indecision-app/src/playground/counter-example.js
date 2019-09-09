836e3b51b0ce6bdc041c6f7864a6faa08124ca0d
eelcire <eelcire.eelcire@hotmail.com>
2019-09-09 09:25:32 -0500

    third commit

--- indecision-app/src/playground/counter-example.js
+++ indecision-app/src/playground/counter-example.js
@@ -5,9 +5,32 @@ class Counter extends React.Component {
         this.handleMinusOne = this.handleMinusOne.bind(this);
         this.handleReset = this.handleReset.bind(this);
         this.state = {
-            count: props.count
+            count: 0
         };
     }
+
+    componentDidMount() {
+        const stringCount = localStorage.getItem('count');
+        const count = parseInt(stringCount, 10);
+
+        if (!isNaN(count)) {
+            this.setState(() => ({ count }))
+        }
+    }
+    componentDidUpdate(prevProps, prevState) {
+        if(prevState.count !== this.state.count) {
+            localStorage.setItem('count', this.state.count);
+        }
+    }
+    // componentDidMoxunt() {
+    //     const json = localStorage.getItem('count');
+    //     const count = JSON.parse(json);
+    //     this.setState(() => ({count}))
+    // }
+    // componentDidUpdate() {
+    //     const json = JSON.stringify(this.state.count);
+    //     localStorage.setItem('count', json)
+    // }
     handleAddOne() {
         this.setState((prevState) => {
             return {
@@ -40,12 +63,6 @@ class Counter extends React.Component {
         )
     }
 }
-
-Counter.defaultProps = {
-    count: 0
-}
-
-
 
 ReactDOM.render(<Counter />, document.getElementById('app'));
 
